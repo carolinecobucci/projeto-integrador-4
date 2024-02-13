a82485eb3a79aedc639f0efe5fbd17e3626bfeb2
Caroline Cobucci <cacobuti@gmail.com>
2024-02-13 17:34:39 -0300

    created places component

---
+++ src/components/Places.jsx
@@ -0,0 +1,17 @@
+const Places = () => {
+  return (
+    <div>
+      <h1>Local de doação</h1>
+      <p>Endereço: Rua tal número tal</p>
+      <p className="mb-3">Cidade: Tal</p>
+      <a
+        href="#"
+        className="border-[#FFC98B] bg-[#FFC98B] hover:bg-[#FFB865] px-4 py-2 rounded-full text-xs"
+      >
+        MAIS INFORMAÇÕES
+      </a>
+    </div>
+  );
+};
+
+export default Places;
