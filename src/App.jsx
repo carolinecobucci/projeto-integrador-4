9f2775c14832989a0685be0d4812c8496d5004d4
Caroline Cobucci <cacobuti@gmail.com>
2024-02-13 17:07:01 -0300

    created home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Home from "./components/Home";
+
 function App() {
-  return <div>APP</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import { IoMdSearch } from "react-icons/io";
+import DonationImage from "../assets/donation-1.jpg";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-6xl font-bold text-gray-500 mb-2">DoeAqui</h1>
+      <h2 className="border-[#FFC98B] bg-[#FFC98B] rounded-md px-2 py-1 text-xs mb-2">
+        APLICATIVO SOLIDÁRIO
+      </h2>
+      <img src={DonationImage} alt="donation image" className="w-52 mb-6" />
+      <form className="flex flex-col justify-center items-center relative">
+        <input
+          type="text"
+          placeholder="Digite sua cidade"
+          className="pl-7 py-1 border rounded-md mb-5"
+        />
+        <IoMdSearch className="absolute left-2 top-2 text-gray-500" />
+        <button className="border-[#FFC98B] bg-[#FFC98B] px-4 py-2 rounded-full text-xs">
+          PROCURAR
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
